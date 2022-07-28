72771fd95b295add924e91976673aaf6c6423e53
Shane Poppleton <shane@bjja.com.au>
2022-07-28 11:16:25 +1000

    Update redis, create redis:flush command

---
+++ app/Console/Commands/FlushRedis.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Redis;
+
+class FlushRedis extends Command
+{
+    protected $signature = 'redis:flush';
+    
+    protected $description = 'Flush Redis Cache';
+
+    public function handle()
+    {
+        Redis::command('flushdb');
+    }
+}
